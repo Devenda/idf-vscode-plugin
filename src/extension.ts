9baca05d73ca9aadc6ea4f07dc2a71c7365a18bb
Tinus Scheppers <tinus_scheppers@telenet.be>
2019-10-18 19:10:45 +0200

    base code for retrieving config and starting menuconfig

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,34 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as ws from 'windows-shortcuts';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log('idf-vscode-plugin active');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "idf-vcode-plugin" is now active!');
+	let terminal = vscode.window.createTerminal("idf-vscode-plugin");
+
+	// Try to get config from ESP-IDF Command Prompt shortcut (installed during official idf tools install)
+	ws.query("C:/Users/tinus/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/ESP-IDF/ESP-IDF Command Prompt (cmd.exe).lnk", (error, options) => {
+		if (options) {
+			console.log(options);
+			console.log("target: " + options.args);
+			console.log("start in: " + options.workingDir);
+		}
+	});
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('extension.idfMenuconfig', () => {
+		console.log('Starting ESP-IDF Command Prompt');
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		terminal.show();
+		terminal.sendText("cd C:/Users/tinus/esp/esp-idf");
+		terminal.sendText("export.bat");
+		terminal.sendText("%IDF_PATH%/tools/idf.py menuconfig");
 	});
 
 	context.subscriptions.push(disposable);

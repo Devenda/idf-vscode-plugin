f580b1d55e92e2a10cd6457a5d7751c6058ef684
Tinus Scheppers <tinus_scheppers@telenet.be>
2019-10-25 17:31:01 +0200

    changes to async getConfig, fix name error

--- src/Config.ts
+++ src/Config.ts
@@ -1,13 +1,101 @@
-export class Config {
-    idf_path: string;
-    idf_tools_path: string;
-    python_path: string;
-    git_path: string;
+import * as vscode from 'vscode';
+import * as find from 'find';
+import * as ws from 'windows-shortcuts';
+import * as util from 'util';
 
-    constructor(idf_path: string, idf_tools_path: string, python_path: string, git_path: string) {
-        this.idf_path = idf_path;
-        this.idf_tools_path = idf_tools_path;
-        this.python_path = python_path;
-        this.git_path = git_path;
+export class Config {
+    public Ready: Thenable<any>;
+
+    idf_path!: string;
+    idf_tools_path!: string;
+    python_path!: string;
+    git_path!: string;
+
+    constructor() {
+        this.Ready = new Promise((resolve, reject) => {
+            this.getConfig().then(() => {
+                resolve(undefined);
+            }).catch(reject);
+        });
+    }
+
+    private async setDefaultConfig() {
+        return new Promise(function (resolve, reject) {// Try to get config from ESP-IDF Command Prompt shortcut (installed during official idf tools install)
+            find.file(/ESP-IDF.*\.lnk/, process.env.APPDATA + "/Microsoft/Windows/Start Menu/Programs", (idfCmdPath) => {
+                if (!idfCmdPath) {
+                    reject("Could not find ESP-IDF Command Prompt in start menu.");
+                }
+                ws.query(idfCmdPath[0], (error, options) => {
+                    if (error) {
+                        throw new Error(error);
+                    }
+                    if (options) {
+                        let idf_path: string | undefined;
+                        let python_path: string | undefined;
+                        let git_path: string | undefined;
+
+                        //Set idf path
+                        idf_path = options.workingDir;
+
+                        //Parse args for python and git paths
+                        if (options.args) {
+                            let args = options.args;
+                            let strippedArgs = args.substring(args.indexOf("\"") + 1, args.lastIndexOf("\""));
+                            let parsedArgs = strippedArgs.match(/(["'])((?=(\\?))\2.)*?\1/g); //https://stackoverflow.com/questions/171480/regex-grabbing-values-between-quotation-marks + change to include backslash
+
+                            if (parsedArgs) {
+                                python_path = parsedArgs[1].replace(/"/g, "");
+                                git_path = parsedArgs[2].replace(/"/g, "");
+                            }
+                        }
+
+                        //Check and store
+                        if (idf_path && python_path && git_path) {
+                            const target = vscode.ConfigurationTarget.Global;
+
+                            let config = vscode.workspace.getConfiguration();
+
+                            config.update('IDF_PATH', idf_path, target);
+                            config.update('PythonPath', python_path, target);
+                            config.update('GitPath', git_path, target);
+
+                            resolve();
+                        } else {
+                            reject("Could not parse ESP-IDF Command Prompt parameters.");
+                        }
+                    }
+                });
+            });
+        });
+    }
+
+    //Retrieve config from vscode and system, if one value is not found return undefined and try te set defaults
+    private async getConfig() {
+        let idf_path: string | undefined;
+        let idf_tools_path: string | undefined;
+        let python_path: string | undefined;
+        let git_path: string | undefined;
+
+        //Read config
+        let config = vscode.workspace.getConfiguration();
+        idf_path = config.get('IDF_PATH');
+        python_path = config.get('PythonPath');
+        git_path = config.get('GitPath');
+
+        //Get from env var
+        idf_tools_path = process.env.IDF_TOOLS_PATH;
+        if (!idf_tools_path) {
+            throw new Error("Unable to find IDF_TOOLS PATH environment variable, is ESP-IDF properly installed?");
+        }
+
+        if (idf_path && python_path && git_path) {
+            this.idf_path = idf_path;
+            this.idf_tools_path = idf_tools_path;
+            this.python_path = python_path;
+            this.git_path = git_path;
+        } else {
+            await this.setDefaultConfig();
+            await this.getConfig();
+        }
     }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,13 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { Uri } from 'vscode';
-import * as ws from 'windows-shortcuts';
-import * as find from 'find';
 
 import { Config } from './Config';
 import { FileSystem } from './FileSystem';
 import { Project } from './Project';
 
 let terminal: vscode.Terminal;
+let config: Config | undefined;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,118 +16,40 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('idf-vscode-plugin active');
 
 	//Get standard terminal
-	terminal = getNewTerminal();
+	try {
+		config = new Config();
+		config.Ready.then(() => {
+			terminal = getNewTerminal(config!);
 
-	//Catch terminal close
-	vscode.window.onDidCloseTerminal((closedTerminal) => {
-		//Only act on close of plugin terminal
-		if (terminal === closedTerminal) {
-			terminal = getNewTerminal();
-		}
-	});
+			//Catch terminal close
+			vscode.window.onDidCloseTerminal((closedTerminal) => {
+				//Only act on close of plugin terminal
+				if (terminal === closedTerminal) {
+					terminal = getNewTerminal(config!);
+				}
+			});
+		});
 
-	//Register commands and status bar icons
-	registerCommandsAndIcons(context);
+
+		//Register commands and status bar icons
+		registerCommandsAndIcons(context);
+	} catch (error) {
+		vscode.window.showErrorMessage("Error initializing ESP-IDF Plugin: " + error);
+	}
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
 
-function setDefaultConfig() {
-	try {
-		// Try to get config from ESP-IDF Command Prompt shortcut (installed during official idf tools install)
-		let idfCmdPath = find.fileSync(/ESP-IDFF.*\.lnk/, process.env.APPDATA + "/Microsoft/Windows/Start Menu/Programs")[0];
-
-		if (!idfCmdPath) {
-			throw new Error("Could not find ESP-IDF Command Prompt in start menu.");
-		}
-
-		ws.query(idfCmdPath, (error, options) => {
-			if (error) {
-				throw new Error(error);
-			}
-			if (options) {
-				let idf_path: string | undefined;
-				let python_path: string | undefined;
-				let git_path: string | undefined;
-
-				//Set idf path
-				idf_path = options.workingDir;
-
-				//Parse args for python and git paths
-				if (options.args) {
-					let args = options.args;
-					let strippedArgs = args.substring(args.indexOf("\"") + 1, args.lastIndexOf("\""));
-					let parsedArgs = strippedArgs.match(/(["'])((?=(\\?))\2.)*?\1/g); //https://stackoverflow.com/questions/171480/regex-grabbing-values-between-quotation-marks + change to include backslash
-
-					if (parsedArgs) {
-						python_path = parsedArgs[1].replace(/"/g, "");
-						git_path = parsedArgs[2].replace(/"/g, "");
-					}
-				}
-
-				//Check and store
-				if (idf_path && python_path && git_path) {
-					const target = vscode.ConfigurationTarget.Global;
-
-					let config = vscode.workspace.getConfiguration();
-
-					config.update('IDF_PATH', idf_path, target);
-					config.update('PythonPath', python_path, target);
-					config.update('GitPath', git_path, target);
-				} else {
-					throw new Error("Could not parse ESP-IDF Command Prompt parameters.");
-				}
-			}
-		});
-	}
-	catch (e) {
-		vscode.window.showErrorMessage("Failed to configure default paths, please set them manually in Settings.\n" + e);
-	}
-}
-
-//Retrieve config from vscode and system, if one value is not found return undefined and try te set defaults
-function getConfig(): Config | undefined {
-	let idf_path: string | undefined;
-	let idf_tools_path: string | undefined;
-	let python_path: string | undefined;
-	let git_path: string | undefined;
-
-	//Read config
-	let config = vscode.workspace.getConfiguration();
-	idf_path = config.get('IDF_PATH');
-	python_path = config.get('PythonPath');
-	git_path = config.get('GitPath');
-
-	//Get from env var
-	idf_tools_path = process.env.IDF_TOOLS_PATH;
-
-	if (idf_path && idf_tools_path && python_path && git_path) {
-		return new Config(idf_path, idf_tools_path, python_path, git_path);
-	} else {
-		setDefaultConfig();
-		return undefined;
-	}
-}
-
-function handleClosedTerminal() {
-	return;
-}
-
-function getNewTerminal(): vscode.Terminal {
+function getNewTerminal(config: Config): vscode.Terminal {
 	//Get terminal
 	let terminal = vscode.window.createTerminal("idf-vscode-plugin", "C:\\Windows\\System32\\cmd.exe");
 
-	//Get config
-	let config: Config | undefined;
-	config = getConfig();
+	//Setup Terminal
 	if (config && terminal) {
 		//Setup terminal, start ESP_IDF command prompt
 		terminal.sendText("cd " + config.idf_path, true);
 		terminal.sendText(config.idf_tools_path + "\\" + "idf_cmd_init.bat " + " \"" + config.python_path + "\" \"" + config.git_path + "\"", true);
-	} else {
-		setDefaultConfig();
-		//TODO: what if it could not be initialized and the commands did not run?
 	}
 
 	return terminal;
@@ -141,8 +62,6 @@ function registerCommandsAndIcons(context: vscode.ExtensionContext) {
 	//Command
 	let idfMenuconfig = 'extension.idfMenuconfig';
 	let idfMenu_command = vscode.commands.registerCommand(idfMenuconfig, () => {
-		if (!terminal) { handleClosedTerminal(); }
-
 		terminal.show();
 		terminal.sendText("cd " + vscode.workspace.rootPath);
 		terminal.sendText("idf.py menuconfig", true);
@@ -162,8 +81,6 @@ function registerCommandsAndIcons(context: vscode.ExtensionContext) {
 	//Command
 	let idfBuild = 'extension.idfBuild';
 	let idfBuild_command = vscode.commands.registerCommand(idfBuild, () => {
-		if (!terminal) { handleClosedTerminal(); }
-
 		terminal.show();
 		terminal.sendText("cd " + vscode.workspace.rootPath);
 		terminal.sendText("idf.py build", true);
@@ -183,8 +100,6 @@ function registerCommandsAndIcons(context: vscode.ExtensionContext) {
 	//Command
 	let idfFlash = 'extension.idfFlash';
 	let idfFlash_command = vscode.commands.registerCommand(idfFlash, () => {
-		if (!terminal) { handleClosedTerminal(); }
-
 		terminal.show();
 		terminal.sendText("cd " + vscode.workspace.rootPath);
 		terminal.sendText("idf.py flash", true);
@@ -205,8 +120,6 @@ function registerCommandsAndIcons(context: vscode.ExtensionContext) {
 	//Command
 	let idfClean = 'extension.idfClean';
 	let idfClean_command = vscode.commands.registerCommand(idfClean, () => {
-		if (!terminal) { handleClosedTerminal(); }
-
 		terminal.show();
 		terminal.sendText("cd " + vscode.workspace.rootPath);
 		cleanProgress();
@@ -227,8 +140,6 @@ function registerCommandsAndIcons(context: vscode.ExtensionContext) {
 	//Command
 	let idfMonitor = 'extension.idfMonitor';
 	let idfMonitor_command = vscode.commands.registerCommand(idfMonitor, () => {
-		if (!terminal) { handleClosedTerminal(); }
-
 		terminal.show();
 		terminal.sendText("cd " + vscode.workspace.rootPath);
 		terminal.sendText("idf.py monitor", true);
@@ -248,7 +159,7 @@ function registerCommandsAndIcons(context: vscode.ExtensionContext) {
 	//Command
 	let idfListExamples = 'extension.idfListExamples';
 	let idfListExamples_command = vscode.commands.registerCommand(idfListExamples, () => {
-		let project = new Project(getConfig()!);
+		let project = new Project(config!);
 		let exampleProjects = project.GetFlatListOfExamples();
 
 		// Ask for example, deploy folder, copies example, changes workspace

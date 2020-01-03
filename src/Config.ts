5f708ffa13fe5abd5af625b68e4470b1d47313d7
Devenda <tinus_scheppers@telenet.be>
2020-01-03 14:39:40 +0100

    Only retrieve idf_path and tools path, ask user if not found

--- src/Config.ts
+++ src/Config.ts
@@ -1,14 +1,13 @@
 import * as vscode from 'vscode';
 import * as find from 'find';
 import * as ws from 'windows-shortcuts';
+import * as os from 'os';
 
 export class Config {
     public Ready: Thenable<any>;
 
     idf_path!: string;
     idf_tools_path!: string;
-    python_path!: string;
-    git_path!: string;
 
     constructor() {
         this.Ready = new Promise((resolve, reject) => {
@@ -19,83 +18,71 @@ export class Config {
     }
 
     private async setDefaultConfig() {
-        return new Promise(function (resolve, reject) {
-            // Try to get config from ESP-IDF Command Prompt shortcut (installed during official idf tools install)
-            find.file(/ESP-IDF.*\.lnk/, process.env.APPDATA + "/Microsoft/Windows/Start Menu/Programs", (idfCmdPath) => {
-                if (!idfCmdPath) {
-                    reject("Could not find ESP-IDF Command Prompt in start menu.");
-                }
-                ws.query(idfCmdPath[0], (error, options) => {
-                    if (error) {
-                        throw new Error(error);
-                    }
-                    if (options) {
-                        let idf_path: string | undefined;
-                        let python_path: string | undefined;
-                        let git_path: string | undefined;
+        let idf_path: string | undefined;
 
-                        //Set idf path
-                        idf_path = options.workingDir;
+        // Test env var
+        if (process.env.IDF_PATH) {
+            idf_path = process.env.IDF_PATH;
+        } else {
+            let noIdfFound = true;
 
-                        //Parse args for python and git paths
-                        if (options.args) {
-                            let args = options.args;
-                            let strippedArgs = args.substring(args.indexOf("\"") + 1, args.lastIndexOf("\""));
-                            let parsedArgs = strippedArgs.match(/(["'])((?=(\\?))\2.)*?\1/g); //https://stackoverflow.com/questions/171480/regex-grabbing-values-between-quotation-marks + change to include backslash
-
-                            if (parsedArgs) {
-                                python_path = parsedArgs[1].replace(/"/g, "");
-                                git_path = parsedArgs[2].replace(/"/g, "");
-                            }
-                        }
-
-                        //Check and store
-                        if (idf_path && python_path && git_path) {
-                            const target = vscode.ConfigurationTarget.Global;
-
-                            let config = vscode.workspace.getConfiguration();
-
-                            config.update('IDF_PATH', idf_path, target);
-                            config.update('PythonPath', python_path, target);
-                            config.update('GitPath', git_path, target);
-
-                            resolve();
-                        } else {
-                            reject("Could not parse ESP-IDF Command Prompt parameters.");
-                        }
-                    }
+            // Ask user to define esp-idf path
+            while (noIdfFound) {
+                let selectedDest = await vscode.window.showOpenDialog({
+                    canSelectFiles: false,
+                    canSelectFolders: true,
+                    canSelectMany: false,
+                    openLabel: "Select esp-idf Folder..."
                 });
-            });
-        });
+
+                if (selectedDest) {
+                    let dir = find.fileSync("export.bat", selectedDest[0].fsPath);
+                    if (dir && dir.length === 1) {
+                        noIdfFound = false;
+                        idf_path = selectedDest[0].fsPath;
+                    } else {
+                        vscode.window.showWarningMessage("Folder does not contain export.bat, please choose another.");
+                        noIdfFound = true;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+
+        // Save config      
+        const target = vscode.ConfigurationTarget.Global;
+        let config = vscode.workspace.getConfiguration();
+
+        await config.update('IDF_PATH', idf_path, target);
     }
 
     //Retrieve config from vscode and system, if one value is not found return undefined and try te set defaults
     private async getConfig() {
         let idf_path: string | undefined;
         let idf_tools_path: string | undefined;
-        let python_path: string | undefined;
-        let git_path: string | undefined;
 
         //Read config
         let config = vscode.workspace.getConfiguration();
         idf_path = config.get('IDF_PATH');
-        python_path = config.get('PythonPath');
-        git_path = config.get('GitPath');
 
         //Get from env var
         idf_tools_path = process.env.IDF_TOOLS_PATH;
         if (!idf_tools_path) {
-            throw new Error("Unable to find IDF_TOOLS PATH environment variable, is ESP-IDF properly installed?");
+            throw new Error("Unable to find IDF_TOOLS_PATH environment variable, is ESP-IDF properly installed?");
         }
 
-        if (idf_path && python_path && git_path) {
+        if (idf_path) {
             this.idf_path = idf_path;
             this.idf_tools_path = idf_tools_path;
-            this.python_path = python_path;
-            this.git_path = git_path;
         } else {
-            await this.setDefaultConfig();
-            await this.getConfig();
+            let option = await vscode.window.showWarningMessage("Could not find esp-idf folder", "Select Folder");
+            if (option === "Select Folder") {
+                await this.setDefaultConfig();
+                await this.getConfig();
+            } else {
+                vscode.window.showWarningMessage("The esp-idf folder is not configured, this folder is needed for the proper functioning of this plugin. The path can also be set in this plugin's settings.");
+            }
         }
     }
 }
